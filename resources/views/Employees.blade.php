3e858fecef7428cfcf22eed0cfadd5a2c0ab6ab9
AyOULAARBI <ayoub6oulaarbi6@gmail.com>
2023-01-25 00:11:20 +0100

    add the html table to lay out the data passed from the EmployeeController to the Employees view

--- resources/views/Employees.blade.php
+++ resources/views/Employees.blade.php
@@ -4,7 +4,33 @@
 @endsection
 
 @section('content')
-    @php
-     hi
-    @endphp
+<div class="container">
+
+  <table border="2px" class="table table-dark text-white">
+    <thead>
+  <tr>
+    <th>id</th>
+    <th>name</th>
+    <th>email</th>
+    <th>address</th>
+    <th>city</th>
+    <th>phone</th>
+   </tr>
+</thead>
+   <tr>
+</tr>
+  
+@foreach($employees as $employee)
+<tbody>
+      <tr>
+        <td>{{$employee->id}}</td>
+        <td>{{$employee->full_name}}</td>
+        <td>{{$employee->email}}</td>
+        <td>{{$employee->address}}</td>
+        <td>{{$employee->city}}</td>
+        <td>{{$employee->phone}}</td>
+      </tr>
+    </tbody>
+      @endforeach
+    </div>
 @endsection
